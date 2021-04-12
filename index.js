cbb1d8969dec89ef99d584c41d5a8e30e04aa8ea
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-04-12 18:24:13 -0300

    testando funçoes e chamada de api externa, formatando o css[teste]

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 import 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js';
-const api = 'https://api.covid19api.com/dayone/country/france';
+
+let api = `https://api.covid19api.com/dayone/country/brazil`;
+
+const country = document.querySelector('.country');
+const btnBusca = document.querySelector('.btn-busca');
+const inputTextCountry = document.querySelector('.countryText');
 
 
 const map_token = "pk.eyJ1Ijoia2FlY2lvIiwiYSI6ImNrbmM5bWs0NDFteGwyc28wcWltd2praDQifQ.88Vcrfzy6vXYP4ZQTcsOLQ"
@@ -12,17 +16,60 @@ zoom:1.5
 });
 
 
-const dataCovid = fetch(`${api}`)
+
+const getCountry = () =>{
+  //var api = `https://api.covid19api.com/dayone/country/brazil`
+  const valueCountry = document.getElementById("countryText").value;
+  if (valueCountry === '' || valueCountry === "Country" || valueCountry === undefined) {
+    api = `https://api.covid19api.com/dayone/country/brazil`
+    console.log('entrou no if')
+    console.log(api)
+  }else if (valueCountry !== '' || valueCountry !== "Country" || valueCountry !== undefined) {
+  
+    api = `https://api.covid19api.com/dayone/country/${valueCountry}`;
+    console.log('entrou no else')
+    console.log(api)
+  }
+  country.innerHTML=  valueCountry;
+}
+
+
+console.log(getCountry())
+
+fetch(`${api}`)
 .then(response => response.json())
 .then(data => {
   const { Country, Deaths, Confirmed, Recovered, Lat, Lon } = data[data.length -1]
+  const country = document.querySelector('.country');
+  const deaths = document.querySelector('.deaths');
+  const confirmed = document.querySelector('.confirmed');
+  const recovered = document.querySelector('.recovered');
+  deaths.innerHTML = Deaths;
+  confirmed.innerHTML = Confirmed;
+  recovered.innerHTML = Recovered;
+  country.innerHTML = Country;
+  
   console.log('País: ',Country)
   console.log('Mortos: ', Deaths)
   console.log('Confirmado: ', Confirmed)
   console.log('Recuperado: ', Recovered)
-  console.log('latitude: ', Lat);
   console.log('longitude: ', Lon);
+  console.log('latitude: ', Lat);
   new mapboxgl.Marker({color: 'red'}).setLngLat([Lon, Lat]).addTo(map);
 });
 
-console.log('ola',dataCovid)
+
+const clearText = () =>{
+  inputTextCountry.innerHTML ='';
+  console.log("teste")
+}
+btnBusca.addEventListener('click',getCountry);
+
+//inputTextCountry.addEventListener('click', clearText);
+
+/*const listCountry =['Brazil', 'Canada']
+for (const pais in listCountry) {
+  country.appendChild(listCountry[pais])
+}
+
+*/  
