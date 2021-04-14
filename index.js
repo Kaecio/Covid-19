1576f91105bbf25a0231a47c80ed406b0bfe00e6
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-04-14 02:01:27 -0300

    refatorando o código

--- index.js
+++ index.js
@@ -1,75 +1,46 @@
+// importando o map do 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css
 import 'https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js';
 
-let api = `https://api.covid19api.com/dayone/country/brazil`;
+// variáveis da DOM
+const btnCountry = document.querySelector('.btn-busca');
 
-const country = document.querySelector('.country');
-const btnBusca = document.querySelector('.btn-busca');
-const inputTextCountry = document.querySelector('.countryText');
-
-
-const map_token = "pk.eyJ1Ijoia2FlY2lvIiwiYSI6ImNrbmM5bWs0NDFteGwyc28wcWltd2praDQifQ.88Vcrfzy6vXYP4ZQTcsOLQ"
+// criando o mapa na tela
+const map_token = 'pk.eyJ1Ijoia2FlY2lvIiwiYSI6ImNrbmM5bWs0NDFteGwyc28wcWltd2praDQifQ.88Vcrfzy6vXYP4ZQTcsOLQ';
 mapboxgl.accessToken = map_token;
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/dark-v10',
-zoom:1.5
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  zoom: 1.5,
 });
 
+// função chama a api externa do Covid e jogando o valor do país no parâmetro
+const getCountry = async (country) => {
+  const url = `https://api.covid19api.com/dayone/country/${country}`;
+  const response = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { Country, Deaths, Confirmed, Recovered, Lat, Lon } = data[data.length - 1];
+      const country = document.querySelector('.country');
+      const deaths = document.querySelector('.deaths');
+      const confirmed = document.querySelector('.confirmed');
+      const recovered = document.querySelector('.recovered');
+      deaths.innerHTML = Deaths;
+      confirmed.innerHTML = Confirmed;
+      recovered.innerHTML = Recovered;
+      country.innerHTML = Country;
 
+      let mapa = new mapboxgl.Marker({ color: 'red' }).setLngLat([Lon, Lat]).addTo(map);
+      console.log(mapa);
+    });
+};
 
-const getCountry = () =>{
-  //var api = `https://api.covid19api.com/dayone/country/brazil`
-  const valueCountry = document.getElementById("countryText").value;
-  if (valueCountry === '' || valueCountry === "Country" || valueCountry === undefined) {
-    api = `https://api.covid19api.com/dayone/country/brazil`
-    console.log('entrou no if')
-    console.log(api)
-  }else if (valueCountry !== '' || valueCountry !== "Country" || valueCountry !== undefined) {
-  
-    api = `https://api.covid19api.com/dayone/country/${valueCountry}`;
-    console.log('entrou no else')
-    console.log(api)
+// evento pega valor e joga na api
+btnCountry.addEventListener('click', () => {
+  const inputCountry = document.querySelector('#countryText').value;
+  if (inputCountry != inputCountry) {
+    window.location.reload();
+    mapa;
   }
-  country.innerHTML=  valueCountry;
-}
-
-
-console.log(getCountry())
-
-fetch(`${api}`)
-.then(response => response.json())
-.then(data => {
-  const { Country, Deaths, Confirmed, Recovered, Lat, Lon } = data[data.length -1]
-  const country = document.querySelector('.country');
-  const deaths = document.querySelector('.deaths');
-  const confirmed = document.querySelector('.confirmed');
-  const recovered = document.querySelector('.recovered');
-  deaths.innerHTML = Deaths;
-  confirmed.innerHTML = Confirmed;
-  recovered.innerHTML = Recovered;
-  country.innerHTML = Country;
-  
-  console.log('País: ',Country)
-  console.log('Mortos: ', Deaths)
-  console.log('Confirmado: ', Confirmed)
-  console.log('Recuperado: ', Recovered)
-  console.log('longitude: ', Lon);
-  console.log('latitude: ', Lat);
-  new mapboxgl.Marker({color: 'red'}).setLngLat([Lon, Lat]).addTo(map);
 });
 
-
-const clearText = () =>{
-  inputTextCountry.innerHTML ='';
-  console.log("teste")
-}
-btnBusca.addEventListener('click',getCountry);
-
-//inputTextCountry.addEventListener('click', clearText);
-
-/*const listCountry =['Brazil', 'Canada']
-for (const pais in listCountry) {
-  country.appendChild(listCountry[pais])
-}
-
-*/  
+getCountry('brazil');
